e4dddf4a0778ac87ab8e9d2091d38fc983762d4e
Deyvison Penha <deyvisonpenha1@gmail.com>
2022-12-16 15:40:33 +0000

    last changes

--- pages/transferList.jsx
+++ pages/transferList.jsx
@@ -1,51 +1,116 @@
-import React, {useState} from "react";
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
-import { ArrowBackIos } from '@mui/icons-material';
-import Button from '@mui/material/Button';
+import React, { useState } from "react";
+
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import { ArrowBackIos, ArrowForwardIos } from "@mui/icons-material";
+import Button from "@mui/material/Button";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import Paper from '@mui/material/Paper';
 
 export default function TransferList() {
   const [lists, setLists] = useState([
-    {title: "Device", items: ["laptop", "smartphone"]},
-    {title: "food", items: ["bread", "chicken"]},
-    {title: "List 1", items: ["item 1", "item2"]}
+    { title: "Device", items: ["laptop", "smartphone"] },
+    { title: "food", items: ["bread", "chicken"] },
+    { title: "List 1", items: ["item 1", "item2"] },
   ]);
 
-  function handleAddItem (index) {
-    const input = window.prompt("Add new item")
+  function handleAddItem(index) {
+    const input = window.prompt("Add new item");
     const newLists = [...lists];
-    newLists[index].items.push(input)
+    newLists[index].items.push(input);
     setLists(newLists);
   }
 
-  function moveItem (item, fromIndex, toIndex) {
+  function moveItem(item, fromIndex, toIndex) {
     const newLists = [...lists];
-    newLists[fromIndex].items = newLists[fromIndex].items.filter(i => i !== item)
+    newLists[fromIndex].items = newLists[fromIndex].items.filter(
+      (i) => i !== item
+    );
     newLists[toIndex].items.unshift(item);
-    setLists(newLists)
+    setLists(newLists);
   }
 
-  return (
-  <Box sx={{display: "flex", gap: 10}} >
-    {lists.map((list,index)=> (
-      <div key={list.title}>
-        <Typography component="h1" sx={{fontSize: "1.5rem", padding: 2, color: "#fff", backgroundColor: "blue"}}>{list.title}</Typography>
-        <ul style={{listStyleType: "none"}}>
-          {list.items.map(item=> (
-            <li key={item}>
-                <button onClick={()=> moveItem(item, index, index-1)}>
-                  <ArrowBackIos color={index > 0 ? "primary" : "disabled"}/>
-                </button>
-              {item}
-              {index < lists.length -1 && (
-                <button onClick={()=> moveItem(item, index, index+1)}>{">"}</button>
-              )}
-            </li>
-          ) )}
-        </ul>
-        <button onClick={()=> handleAddItem(index)}>Add Item</button>
-      </div>
-    ))}
-  </Box>
-  )
+  const randColor = () =>  {
+    return "#" + Math.floor(Math.random()*16777215).toString(16).padStart(6, '0').toUpperCase();
 }
+
+  return (
+    <Box sx={{ display: "flex", gap: 10 }}>
+      {lists.map((list, index) => (
+        <Paper elevation={4} sx={{display:"flex", flexDirection: "column", width: 400 }} key={list.title}>
+          <Typography
+            component="h1"
+            sx={{
+              fontSize: "1.5rem",
+              py: 1,
+              color: "#fff",
+              backgroundColor: randColor(),
+              textAlign: "center"
+            }}
+          >
+            {list.title}
+          </Typography>
+          <List>
+            {list.items.map((item) => (
+              <ListItem
+                key={item}
+                sx={{
+                  display: "flex",
+                  flexGrow: 1,
+                  justifyContent: "space-between",
+                  px: 0,
+                }}
+              >
+                <Button
+                  variant="text"
+                  sx={{ padding: 0 }}
+                  onClick={() =>
+                    index > 0 ? moveItem(item, index, index - 1) : null
+                  }
+                >
+                  <ArrowBackIos
+                    fontSize="small"
+                    color={index > 0 ? "primary" : "disabled"}
+                  />
+                </Button>
+                <Typography
+                  component="h1"
+                  sx={{
+                    fontSize: "1rem",
+                  }}
+                >
+                  {item}
+                </Typography>
+
+                <Button
+                  variant="text"
+                  sx={{ padding: 0 }}
+                  onClick={() =>
+                    index < lists.length - 1
+                      ? moveItem(item, index, index + 1)
+                      : null
+                  }
+                >
+                  <ArrowForwardIos
+                    fontSize="small"
+                    color={index < lists.length - 1 ? "primary" : "disabled"}
+                  />
+                </Button>
+              </ListItem>
+            ))}
+          </List>
+          <Button sx={{justifySelf: "center", mx: 6, my: 3}} variant="outlined" onClick={() => handleAddItem(index)}>+ Add a Task</Button>
+        </Paper>
+      ))}
+    </Box>
+  );
+}
+
+// <Button variant="text" onClick={()=> moveItem(item, index, index-1)}>
+//                   <ArrowBackIos fontSize="small" color={index > 0 ? "primary" : "disabled"}/>
+//                 </Button>
+//               {item}
+//                 <Button variant="text" onClick={()=>  moveItem(item, index, index-1)}>
+//                   <ArrowForwardIos fontSize="small" color={index < lists.length-1 ? "primary" : "disabled"}/>
+//                 </Button>
